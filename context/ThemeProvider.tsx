dfb12bd7846bc5a487592b4c9d49ff0cc21c5a00
Rushikesh Gandhmal <rushikeshgandhmal@gmail.com>
2024-02-08 23:46:05 +0530

    fix infinite

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -24,7 +24,7 @@ const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
 
   useEffect(() => {
     handleThemeChange();
-  }, [mode]);
+  }, []);
 
   return (
     <ThemeContext.Provider value={{ mode, setMode }}>
