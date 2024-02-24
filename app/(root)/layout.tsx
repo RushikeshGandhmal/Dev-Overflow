747d1d72c2def36cb8f572ff7212017c2a19b47a
Rushikesh Gandhmal <rushikeshgandhmal@gmail.com>
2024-02-24 23:23:28 +0530

    Implement LeftSidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import LeftSidebar from "@/components/shared/LeftSidebar";
 import NavBar from "@/components/shared/navbar/NavBar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
@@ -5,7 +6,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <main className="background-light850_dark100 relative">
       <NavBar />
       <div className="flex">
-        LeftSide
+        <LeftSidebar />
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
